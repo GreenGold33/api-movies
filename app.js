0263b28cf431e2179f707d4b79b669c7e85fe7df
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2016-11-11 11:45:51 +0100

    genre & countries endpoints

--- app.js
+++ app.js
@@ -13,22 +13,81 @@ MongoClient.connect(url, (err, db) => {
 
 	app.get('/movies', (req, res) => {
 
-		const limit = 10;
-		const page = 3;
-		const skip = limit*page;
+		const limit = +req.query.size || 20;
+		const page = +req.query.page || 1;
+		const skip = (limit*(page-1))+1;
 
-		db.collection("movies")
-			.find( { }, { title: 1, _id: 0 } )
+		// (page === 1) ==> from 1;
+		// (page === 2) ==> from 21;
+		// (page === 3) ==> from 41;
+
+		db.collection("movieDetails")
+			.find( { } /* , { title: 1, _id: 0 } */ )
 			.sort( { title:1 } )
-			.limit( 5 )
+			.limit( limit )
 			.skip( skip )
 			.toArray()
 			//.then( movies => movies.map( m => m.title )  )
 			.then( data => res.json(data) )
-			.then( () => db.close() )
+			//.then( () => db.close() )
+			.catch( err => console.log(err) )
+
+	})
+
+	// /movies/genres/Comedy
+	// /movies/genres/Thriller
+	// /movies/genres/Action
+
+	app.get('/movies/genres/:genre', (req, res) => {
+
+		const limit = +req.query.size || 20;
+		const page = +req.query.page || 1;
+		const skip = (limit*(page-1))+1;
+
+		const genre = req.params.genre;
+
+		// (page === 1) ==> from 1;
+		// (page === 2) ==> from 21;
+		// (page === 3) ==> from 41;
+
+		db.collection("movieDetails")
+			.find( { genres: genre } /* , { title: 1, _id: 0 } */ )
+			.sort( { title:1 } )
+			.limit( limit )
+			.skip( skip )
+			.toArray()
+			//.then( movies => movies.map( m => m.title )  )
+			.then( data => res.json(data) )
+			//.then( () => db.close() )
+			.catch( err => console.log(err) )
+
+	})
+
+	app.get('/movies/countries/:country', (req, res) => {
+
+		const limit = +req.query.size || 20;
+		const page = +req.query.page || 1;
+		const skip = (limit*(page-1))+1;
+
+		const country = req.params.country;
+
+		// (page === 1) ==> from 1;
+		// (page === 2) ==> from 21;
+		// (page === 3) ==> from 41;
+
+		db.collection("movieDetails")
+			.find( { countries: country } /* , { title: 1, _id: 0 } */ )
+			.sort( { title:1 } )
+			.limit( limit )
+			.skip( skip )
+			.toArray()
+			//.then( movies => movies.map( m => m.title )  )
+			.then( data => res.json(data) )
+			//.then( () => db.close() )
+			.catch( err => console.log(err) )
 
 	})
 
 })
 
-app.listen(PORT, () => `Listening on port ${PORT}...`)
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`) )
